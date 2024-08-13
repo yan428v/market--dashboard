758a56a85cb642d891499cdbb6fa7d6633592df6
yan <yan428@mail.ru>
2024-08-13 14:03:21 +0300

    add pm2 config file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     esbuild: {
         supported: {
-            'top-level-await': true //browsers can handle top-level-await features
+            'top-level-await': true
         },
     },
     optimizeDeps: {
